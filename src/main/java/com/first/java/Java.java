6800d3e1271067cdf6016f5a258884832f311a82
Daniel Lubiejewski <lubiejewskidaniel@gmail.com>
2024-10-19 13:10:48 +0100

    first

---
+++ src/main/java/com/first/java/Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.first.java;
+
+/**
+ *
+ * @author lubie
+ */
+public class Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
