0eca9e8ba6d7e0bac8959d49f656685ad0da9570
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2024-10-19 13:33:06 +0100

    third

--- src/main/java/com/first/java/Java.java
+++ src/main/java/com/first/java/Java.java
@@ -13,5 +13,7 @@ public class Java {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World Daniel !!!");
+        System.out.println("Hello World Daniel Piotr Lubiejewski!!!");
+        
     }
 }
