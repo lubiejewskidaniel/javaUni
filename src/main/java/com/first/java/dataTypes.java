f8161223b5b5d63423c319d712da1560781e687a
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2024-10-20 08:39:52 +0100

    data types and variables

---
+++ src/main/java/com/first/java/dataTypes.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.first.java;
+
+/**
+ *
+ * @author lubie
+ */
+public class dataTypes {
+    /*
+    Data Types and Variables
+    Java has several built-in data types, including:
+    byte: 8-bit integer
+    short: 16-bit integer
+    int: 32-bit integer
+    long: 64-bit integer
+    float: 32-bit floating-point number
+    double: 64-bit floating-point number
+    boolean: true or false
+    char: single character
+
+    To declare a variable in Java, you need to specify its data type and name. 
+    For example:
+    int myNumber = 42;
+    double myDouble = 3.14;
+    boolean isTrue = true;
+    char myChar = 'a';
+
+    */
+}
