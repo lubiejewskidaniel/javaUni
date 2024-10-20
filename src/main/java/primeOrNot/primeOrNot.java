1a807a620fafe4d0df1d9a91e5efb4b1715250ef
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2024-10-20 12:11:10 +0100

    Prime or Not

---
+++ src/main/java/primeOrNot/primeOrNot.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package primeOrNot;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lubie
+ */
+public class primeOrNot {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }    
+    }
+        
+}
