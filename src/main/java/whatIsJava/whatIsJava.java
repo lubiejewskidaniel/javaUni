0a002a20162a538a10c66ed7fd2adc12ca339e04
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2024-10-20 10:39:22 +0100

    files reorganizing

---
+++ src/main/java/whatIsJava/whatIsJava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package whatIsJava;
+
+/**
+ *
+ * @author lubie
+ */
+public class whatIsJava {
+    public static void main(String[] args) {
+    /*
+    What is Java?
+    Java is a general-purpose programming language that was created in 1995 
+    by Sun Microsystems. It is an object-oriented language that is designed 
+    to be platform-independent, which means that Java code can run on any 
+    device or operating system that has a Java Virtual Machine (JVM) installed.
+    */
+    }
+}
