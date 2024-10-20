8df1071fd0037af9cec1597b7e6ac2161dca09b7
DanielLubiejewski <lubiejewskidaniel@gmail.com>
2024-10-20 08:42:27 +0100

    Control flow statements

---
+++ src/main/java/com/first/java/controlFlowStatements.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.first.java;
+
+/**
+ *
+ * @author lubie
+ */
+public class controlFlowStatements {
+/*
+    Control Flow Statements
+    Java has several control flow statements that allow you to control the flow 
+    of execution in your program. These include:
+    - if/else statements: used to make decisions based on a condition
+    - for loops: used to repeat a block of code a certain number of times
+    - while loops: used to repeat a block of code while a condition is true
+    - switch statements: used to choose between several options based on a value
+
+Here is an example of an if/else statement in Java:
+
+int myNumber = 42;
+
+if (myNumber > 0) {
+    System.out.println("The number is positive.");
+} else if (myNumber < 0) {
+    System.out.println("The number is negative.");
+} else {
+    System.out.println("The number is zero.");
+}
+In this example, we check if the variable "myNumber" is greater than 0. 
+If it is, we print the message "The number is positive.". If it is not, 
+we check if it is less than 0. If it is, we print the message 
+"The number is negative.". If it is neither greater than nor less than 0, 
+we print the message "The number is zero.".
+
+*/
+}
